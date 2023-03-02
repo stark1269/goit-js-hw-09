b395ca7409f785284c48e0dff37e811934347cbf
stark1269 <stark1269@gmail.com>
2023-03-02 13:26:16 +0200

    add task one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+let timeId = null;
+refs.stop.disabled = true;
+
+function setBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+};
+
+function onClickStart() {
+  timeId = setInterval(setBodyColor, 1000);
+  refs.start.disabled = true;
+  refs.stop.disabled = false;
+};
+
+function onClickStop() { 
+  clearInterval(timeId);
+  refs.stop.disabled = true;
+  refs.start.disabled = false;
+};
+
+refs.start.addEventListener('click', onClickStart);
+refs.stop.addEventListener('click', onClickStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
